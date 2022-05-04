9168c1f765369400ccab96693319f4ac9f42819a
jorgeboris <eduardo.borisq@gmail.com>
2022-05-03 23:37:59 -0500

    Haciendo que la segunda prueba falle

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,8 +2,13 @@ const Reader = require("./../../lib/utils/Reader");
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Test para ExplorerController", () => {
-    test("Buscar explorers con getExplorersByMission", () => {
+    test("Rquerimiento 1: Buscar explorers con getExplorersByMission", () => {
         const explorers = Reader.readJsonFile("explorers.json");
         expect(ExplorerController.getExplorersByMission(explorers, "node").length).toBeGreaterThanOrEqual(0);
+    });
+
+    test("Requerimiento 2: Obtener usernames de explorers con getExplorerUsernameByMission", () => {
+        const explorersUsernames = ExplorerController.getExplorerUsernamesByMission("node")
+        expect(explorersUsernames).toContain("ajolonautaaa14");
     });
 });
